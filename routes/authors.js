eb023c72e3089f59d487921d1d682d5b90f8ab02
emprexvest <emprexvest@gmail.com>
2023-02-15 17:13:18 -0800

    Books Create/Index/New

--- routes/authors.js
+++ routes/authors.js
@@ -1,56 +1,44 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const Author = require('../models/author')
 
-//All Authors Route
+// All Authors Route
 router.get('/', async (req, res) => {
-    let searchOptions = {};
+    let searchOptions = {}
     if (req.query.name != null && req.query.name !== '') {
-        searchOptions.name = new RegExp(req.query.name, 'i');
+        searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try {
-        const authors = await Author.find(searchOptions);
+        const authors = await Author.find(searchOptions)
         res.render('authors/index', {
             authors: authors, 
             searchOptions: req.query
-        });
+        })
     } catch {
-        res.redirect('/');
+        res.redirect('/')
     }
-});
+})
 
 // New Author Route
 router.get('/new', (req, res) => {
     res.render('authors/new', { author: new Author() })
-});
+})
 
 // Create Author Route
 router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
-    });
+    })
     try {
         const newAuthor = await author.save()
-        //res.redirect(`authors/${newAuhtor.id`)
-        res.redirect('authors');
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
     } catch {
         res.render('authors/new', {
         author: author,
-        errorMessage: 'Error Creating Author'
-        });
+        errorMessage: 'Error creating Author'
+        })
     }
+})
 
-    // author.save((err, newAuthor) => {
-    //     if (err) {
-    //         res.render('authors/new', {
-    //             author: author,
-    //             errorMessage: 'Error Creating Author'
-    //         }) 
-    //     } else {
-    //         //res.redirect(`authors/${newAuhtor.id`)
-    //         res.redirect('authors')
-    //     }
-    // })
-});
-
-module.exports = router;
+module.exports = router
