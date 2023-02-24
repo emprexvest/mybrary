df81d29a7063e3432186fb7298588be4aedd16b8
emprexvest <emprexvest@gmail.com>
2023-02-24 12:02:36 -0800

    File Upload Database

--- models/book.js
+++ models/book.js
@@ -1,5 +1,4 @@
-const mongoose = require('mongoose')
-const path = require('path')
+const mongoose = require('mongoose');
 const coverImageBasePath = 'uploads/bookCovers';
 
 const bookSchema = new mongoose.Schema({
@@ -23,7 +22,11 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+    },
+    coverImageType: {
         type: String,
         required: true
     },
@@ -32,13 +35,13 @@ const bookSchema = new mongoose.Schema({
         required: true,
         ref: 'Author'
     }
-})
+});
 
 bookSchema.virtual('coverImagePath').get(function() {
-if (this.coverImageName != null) {
-    return path.join('/', coverImageBasePath, this.coverImageName)
-    }
-})
+if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+}
+});
 
-module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
+module.exports = mongoose.model('Book', bookSchema);
+module.exports.coverImageBasePath = coverImageBasePath;
